baa2b2312ac883918981048d14bf3ef185294d75
house <housesellout@gmail.com>
2023-11-07 16:02:19 -0500

    :- added _dode_customer_error.py (not sure if its working correctly) 	- initially added to decode BalancerFLR_pc.sol output from require() 		output: 0x4e487b7100000000000000000000000000000000000000000000000000000000000000000000000000000000000000011 	- out should be from checking the final balance of loan token 		just before the loan payback occurs 	- getting ’17’ as output from the end part 	- getting ‘NH{q’ as output for first part (function name i think) 	- not sure if its working, not sure if 17 makes sense as a ‘balance’
    NEXT:
    	- rework arb_search algorithm to generate fuller paths 
    		from WETH to WETH on pulsechain searches
    		 (in order to take advantage of the low value arb opps)
    	- start testing BalancerFLR_pc on ethereum main net
    		using simple direct paths currently generated from arb_search
    		 (since we now know the .sol algorithm works)

---
+++ src/_decode_custom_error.py
@@ -0,0 +1,42 @@
+import re
+import binascii
+
+hex_message = "0x4e487b710000000000000000000000000000000000000000000000000000000000000011"
+
+# Remove the "0x" prefix
+hex_message = hex_message[2:]
+
+# Convert the hex message to bytes
+message_bytes = bytes.fromhex(hex_message)
+
+# Find the position of the string in the message
+string_pattern = re.compile(b'\x00[^\x00]*\x00')
+match = string_pattern.search(message_bytes)
+
+if match:
+    string_start = match.start()
+    string_end = match.end()
+
+    # Decode the string data as UTF-8 (note_110623: attempt failed)
+    string_data = message_bytes[string_start:string_end]
+    err_msg_string_data = string_data[1:-1].decode('utf-8')
+    
+    # Decode the entire message as UTF-8 (note_110623: attempt failed)
+    err_msg_message_bytes = message_bytes[1:-1].decode('utf-8')
+
+    # Decode just the function name (note_110623: attempt failed)
+    func_name_data = message_bytes[:string_start]
+    func_name = func_name_data.decode('utf-8')
+    
+    # The amount is stored as a byte array, convert it to an integer
+    amount_data = message_bytes[string_end:]
+    amount_owed = int.from_bytes(amount_data, byteorder='big')
+
+    print(f' string_start: {string_start}\n string_end: {string_end}')
+    print("Error Message (message_bytes):", err_msg_message_bytes)
+    print("Error Message (string_data):", err_msg_string_data)
+    print("Function Name:", func_name)
+    print("Amount Owed:", amount_owed)
+else:
+    print("Error: Dynamic data not found in the message")
+
